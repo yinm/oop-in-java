ffba59df91f71f47aae4a273ebac52a0643d8fcf
yinm <yinm.cminor9@gmail.com>
2019-02-04 09:02:40 +0900

    Add 4-2

--- src/Lesson4/Murata.java
+++ src/Lesson4/Murata.java
@@ -1,51 +1,11 @@
 package Lesson4;
 
-public class Murata {
-    public static final int STONE = 0;
-    public static final int SCISSORS = 1;
-    public static final int PAPER = 2;
-
-    private String name_;
-    private int winCount_ = 0;
-
+public class Murata extends Player {
     public Murata(String name) {
-        this.name_ = name;
+        super(name);
     }
 
-    /**
-     * @return ジャンケンの手
-     */
     public int showHand() {
-        double randomNum = Math.random() * 3;
-
-        if (randomNum < 1) {
-            return STONE;
-        } else if (randomNum < 2) {
-            return SCISSORS;
-        } else if (randomNum < 3) {
-            return PAPER;
-        }
-
-        return 0;
-    }
-
-    /**
-     * @param result true: 勝ち, false: 負け
-     */
-    public void notifyResult(boolean result) {
-        if (result) {
-            this.winCount_++;
-        }
-    }
-
-    /**
-     * @return 勝った回数
-     */
-    public int getWinCount() {
-        return this.winCount_;
-    }
-
-    public String getName() {
-        return name_;
+        return STONE;
     }
 }
--- src/Lesson4/ObjectJanken.java
+++ src/Lesson4/ObjectJanken.java
@@ -4,8 +4,8 @@ public class ObjectJanken {
     public static void main(String[] args) {
         Judge saito = new Judge();
 
-        Murata murata = new Murata("村田さん");
-        Yamada yamada = new Yamada("山田さん");
+        Player murata = new Murata("村田さん");
+        Player yamada = new Yamada("山田さん");
 
         saito.startJanken(murata, yamada);
     }
