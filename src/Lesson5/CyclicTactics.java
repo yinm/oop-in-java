877df37e5b016916978638d777803cc8ec9a65b3
yinm <yinm.cminor9@gmail.com>
2019-02-05 09:16:35 +0900

    Check

--- src/Lesson5/CyclicTactics.java
+++ src/Lesson5/CyclicTactics.java
@@ -1,20 +1,11 @@
 package Lesson5;
 
 public class CyclicTactics implements Tactics {
-    private int battleCount_ = 0;
+    private int lastHand = -1;
 
     public int readTactics() {
-        int hand = 0;
+        lastHand = (lastHand + 1) % 3;
 
-        if (battleCount_ % 3 == 0) {
-            hand = Player.STONE;
-        } else if (battleCount_ % 3 == 1) {
-            hand = Player.SCISSORS;
-        } else if (battleCount_ % 3 == 2) {
-            hand = Player.PAPER;
-        }
-
-        battleCount_++;
-        return hand;
+        return lastHand;
     }
 }
